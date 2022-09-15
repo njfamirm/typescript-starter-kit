8c714c8bc9f6ee7ef3267298dd7d969d8a2df292
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-09-15 21:32:25 +0430

    feat: add web-dev-server

---
+++ dev-server.mjs
@@ -0,0 +1,14 @@
+// https://modern-web.dev/docs/dev-server/cli-and-configuration/#configuration-file
+export default {
+  open: false,
+  nodeResolve: true,
+  watch: true,
+  // appIndex: './',
+  // rootDir: '.',
+  // debug: false,
+  hostname: '0.0.0.0',
+  port: 8000,
+  preserveSymlinks: true,
+  plugins: [],
+  middleware: [],
+};
