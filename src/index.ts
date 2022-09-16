8597b0975540e577cab8a5f1998a068aee0fb140
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2022-09-16 14:49:30 +0430

    feat: add simple lit element

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello world!');
+import {AlwatrElement} from '@alwatr/element';
+import {html, css, CSSResultGroup} from 'lit';
+import {customElement} from 'lit/decorators/custom-element.js';
+
+import type {TemplateResult} from 'lit';
+
+@customElement('alwatr-element')
+export class Phrase extends AlwatrElement {
+  static override styles?: CSSResultGroup = [
+    css`
+    * {
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+    `,
+  ];
+
+  override render(): TemplateResult {
+    return html`
+    <p dir=ltr>Hello world!</p>
+    <p>سلام دنیا!</p>
+    `;
+  }
+}
